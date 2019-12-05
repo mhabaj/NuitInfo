c6844c307b23d16352119c7e1e9ab5ea6c469108
mhabaj <mhabaj99@hotmail.com>
2019-12-05 23:38:06 +0100

    User class

---
+++ BDD/sqlV2.sql
@@ -0,0 +1,60 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: User
+#------------------------------------------------------------
+
+CREATE TABLE User(
+        idUser   Int  Auto_increment  NOT NULL ,
+        userName Varchar (15) NOT NULL ,
+        password Varchar (255) NOT NULL ,
+        status   TinyINT NOT NULL
+	,CONSTRAINT User_PK PRIMARY KEY (idUser)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Stream
+#------------------------------------------------------------
+
+CREATE TABLE Stream(
+        idStream    Int  Auto_increment  NOT NULL ,
+        streamVideo Varchar (255) NOT NULL ,
+        streamChat  Varchar (255) NOT NULL
+	,CONSTRAINT Stream_PK PRIMARY KEY (idStream)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: send
+#------------------------------------------------------------
+
+CREATE TABLE send(
+        idStream      Int NOT NULL ,
+        idUser        Int NOT NULL ,
+        MessageDate   Datetime NOT NULL ,
+        messageStatus Bool NOT NULL ,
+        message       Varchar (255) NOT NULL
+	,CONSTRAINT send_PK PRIMARY KEY (idStream,idUser)
+
+	,CONSTRAINT send_Stream_FK FOREIGN KEY (idStream) REFERENCES Stream(idStream)
+	,CONSTRAINT send_User0_FK FOREIGN KEY (idUser) REFERENCES User(idUser)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: moderate
+#------------------------------------------------------------
+
+CREATE TABLE moderate(
+        idStream Int NOT NULL ,
+        idUser   Int NOT NULL
+	,CONSTRAINT moderate_PK PRIMARY KEY (idStream,idUser)
+
+	,CONSTRAINT moderate_Stream_FK FOREIGN KEY (idStream) REFERENCES Stream(idStream)
+	,CONSTRAINT moderate_User0_FK FOREIGN KEY (idUser) REFERENCES User(idUser)
+)ENGINE=InnoDB;
+
