ecb984fbd0ce6b7f8faa4a6350ef8ec4f53ecf0b
Emeric <emeric.verrier@etu.univ-tours.fr>
2019-12-06 00:17:57 +0100

    some changes with include.php

---
+++ formulaireConnexion.php
@@ -0,0 +1,19 @@
+<?php 
+include "header.html";
+include "navbar.html";
+?>
+<div class="container">
+            <div class="jumbotron">
+                    <div class="form-group">
+                        <form method="POST" action="">
+                        <input class="form-control" type="text" id="login" placeholder="your login"/><label for="login">your login</label>
+                        <input class="form-control" type="password" id="password" placeholder="your password"/><label for="password">your password</label>
+                        <div class="row justify-content-center">
+                            <span class="text-center">
+                                <input class="btn btn-primary text-center" type="submit" value="submit">
+                            </span>
+                        </div>
+                        </form>
+                    </div>
+            </div>
+</div>
